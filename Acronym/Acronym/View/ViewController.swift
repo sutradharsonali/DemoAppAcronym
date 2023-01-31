5fc390558ef4c844280c1f2402fa3b9739ef4dc6
troy <titas@optimizeitsystems.com>
2023-01-31 16:40:16 +0530

    Second Commit

--- Acronym/Acronym/View/ViewController.swift
+++ Acronym/Acronym/View/ViewController.swift
@@ -8,6 +8,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tableview : UITableView!
     @IBOutlet weak var txtFieldSearchValue : UITextField!
+    @IBOutlet weak var lblAlert : UILabel!
     let acronymViewModel = AcronymViewModel()
    
 
@@ -19,14 +20,41 @@ class ViewController: UIViewController {
 
         acronymViewModel.client = HttpUtility()
         acronymViewModel.acronyms.bind { [weak self]_ in
-            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: {
-                self?.tableview.reloadData()
-            })
+            if ((self?.acronymViewModel.acronyms.value?.isEmpty) != nil){
+                DispatchQueue.main.async {
+                    self?.tableview.isHidden = false
+                    self?.tableview.reloadData()
+                    self?.lblAlert.isHidden = true
+                }
+            }else{
+                DispatchQueue.main.async {
+                    self?.tableview.isHidden = true
+                    self?.tableview.reloadData()
+                    self?.lblAlert.isHidden = false
+                    if self?.txtFieldSearchValue.text == "" {
+                        self?.lblAlert.text = "Type to Search Acronym"
+                    }
+                    else
+                    {
+                        self?.lblAlert.text = "No Acronym found"
+                    }
+                    
+                }
+            }
         }
     }
     
     @objc func textFieldDidChange(){
-        acronymViewModel.getAcronymRecords(searchValue: self.txtFieldSearchValue.text ?? "") 
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: {
+            if self.txtFieldSearchValue.text == ""{
+                self.acronymViewModel.acronyms.value = nil
+                
+            }else{
+                self.acronymViewModel.getAcronymRecords(searchValue: self.txtFieldSearchValue.text ?? "")
+            }
+        })
+        
     }
 }
 
